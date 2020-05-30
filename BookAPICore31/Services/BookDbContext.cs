33fd3d3b4ad74cccf7a5426c12540ec8af003474
Arun Thomas <ArunThomas04@gmail.com>
2020-05-30 07:39:51 +0530

    Added Migration

--- BookAPICore31/Services/BookDbContext.cs
+++ BookAPICore31/Services/BookDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookAPICore31.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,52 @@ namespace BookAPICore31.Services
 {
     public class BookDbContext : DbContext
     {
+        public BookDbContext(DbContextOptions<BookDbContext> options) 
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.CategoryId });
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne<Book>(bc => bc.Book)
+                .WithMany(b => b.BookCategories)
+                .HasForeignKey(bc => bc.BookId);
+            modelBuilder.Entity<BookCategory>()
+                .HasOne<Category>(bc => bc.Category)
+                .WithMany(c => c.BookCategories)
+                .HasForeignKey(bc => bc.CategoryId);
+
+
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new {ba.BookId, ba.AuthorId});
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne<Book>(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne<Author>(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
+            
+
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- BookAPICore31/Startup.cs
+++ BookAPICore31/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookAPICore31.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace BookAPICore31
             services.AddMvc();
 
             var connString = _Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<BookDbContext>(c => c.UseSqlServer(connString));
 
         }
 
